8fa9e019f9dcb1682d7fdd0992a75e8fb69028b4
Dishant <tdishant27@gmail.com>
2021-09-28 22:27:24 +0530

    Python program for First and Follow

---
+++ first_and_follow.py
@@ -0,0 +1,133 @@
+import re
+
+def cal_follow(s, productions, first):
+    follow = set()
+    if len(s)!=1 :
+        return {}
+    if(s == list(productions.keys())[0]):
+        follow.add('$') 
+    
+    for i in productions:
+        for j in range(len(productions[i])):
+            if(s in productions[i][j]):
+                idx = productions[i][j].index(s)
+                
+                if(idx == len(productions[i][j])-1):
+                    if(productions[i][j][idx] == i):
+                        break
+                    else:
+                        f = cal_follow(i, productions, first)
+                        for x in f:
+                            follow.add(x)
+                else:
+                    while(idx != len(productions[i][j]) - 1):
+                        idx += 1
+                        if(not productions[i][j][idx].isupper()):
+                            follow.add(productions[i][j][idx])
+                            break
+                        else:
+                            f = cal_first(productions[i][j][idx], productions)
+                            
+                            if('ε' not in f):
+                                for x in f:
+                                    follow.add(x)
+                                break
+                            elif('ε' in f and idx != len(productions[i][j])-1):
+                                f.remove('ε')
+                                for k in f:
+                                    follow.add(k)
+                            
+                            elif('ε' in f and idx == len(productions[i][j])-1):
+                                f.remove('ε')
+                                for k in f:
+                                    follow.add(k)
+                                
+                                f = cal_follow(i, productions, first)
+                                for x in f:
+                                    follow.add(x)
+                            
+    return follow
+   
+def cal_first(s, productions):
+    
+    first = set()
+    
+    for i in range(len(productions[s])):
+        
+        for j in range(len(productions[s][i])):
+            
+            c = productions[s][i][j]
+            
+            if(c.isupper()):
+                f = cal_first(c, productions)
+                if('ε' not in f):
+                    for k in f:
+                        first.add(k)
+                    break
+                else:
+                    if(j == len(productions[s][i])-1):
+                        for k in f:
+                            first.add(k)
+                    else:
+                        f.remove('ε')
+                        for k in f:
+                            first.add(k)
+            else:
+                first.add(c)
+                break
+                
+    return first
+                       
+def main():
+    productions = {}
+    grammar = open("grammar9.txt", "r")
+    
+    first = {}
+    follow = {}
+    
+    for prod in grammar:
+        l = re.split("( /->/\n/)*", prod)
+        m = []
+        for i in l:
+            if (i == "" or i == None or i == '\n' or i == " " or i == "-" or i == ">"):
+                pass
+            else:
+                m.append(i)
+        
+        left_prod = m.pop(0)
+        right_prod = []
+        t = []
+        
+        for j in m:
+            if(j != '|'):
+                t.append(j)
+            else:
+                right_prod.append(t)
+                t = []
+        
+        right_prod.append(t)
+        productions[left_prod] = right_prod
+    
+    for s in productions.keys():
+        first[s] = cal_first(s, productions)
+    
+    print("*****FIRST*****")
+    for lhs, rhs in first.items():
+        print(lhs, ":" , rhs)
+    
+    print("")
+    
+    for lhs in productions:
+        follow[lhs] = set()
+    
+    for s in productions.keys():
+        follow[s] = cal_follow(s, productions, first)
+    
+    print("*****FOLLOW*****")
+    for lhs, rhs in follow.items():
+        print(lhs, ":" , rhs)
+    
+    grammar.close()
+
+if __name__ == "__main__":
+    main()
